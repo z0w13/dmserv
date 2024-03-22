0ca66f5e14deffddb5733632d03f94d287aa2651
Zowie <git@zowie.cx>
2024-03-22 18:31:02 +0100

    fix: remove extra space in alter role names

--- src/dmserv/cogs/main.py
+++ src/dmserv/cogs/main.py
@@ -122,7 +122,7 @@ class MainCog(commands.Cog):
 
         # TODO: Don't do basic bitch splitting to remove pronouns but something more robust
         desired_alter_roles = {
-            n.split("(")[0] + " (Alter)" for n in await get_display_names(pk)
+            n.split(" (")[0] + " (Alter)" for n in await get_display_names(pk)
         }
         current_alter_roles = set(role_dict.keys())
 
